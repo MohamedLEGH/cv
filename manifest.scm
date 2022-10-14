c0b7efe285cf2e1451f4ea22a7963f63d5745f85
Mohamed Amine LEGHERABA <mlegheraba@protonmail.com>
2022-10-14 10:15:05 +0200

    guix manifest

---
+++ manifest.scm
@@ -0,0 +1,23 @@
+(specifications->manifest
+ '(
+   "make"
+   "rubber"
+
+   "texlive-base"
+   "texlive-ragged2e"
+   "texlive-everysel"
+   "texlive-latex-footmisc"
+   "texlive-etoolbox"
+   "texlive-tools"
+   "texlive-fonts-latex"
+   "texlive-fonts-ec"
+  ; "texlive-inputenc"
+  ; "texlive-longtable"
+   "texlive-enumitem"
+   "texlive-latex-parskip"
+  ; "textlive-array"
+   "texlive-xcolor"
+  ; "texlive-graphicx"
+   "texlive-latex-geometry"
+   "texlive-hyperref"
+))
